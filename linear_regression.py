8968f14716243ac3b837b449f42e8fd789e16905
rehan-aiml <mdrehan50862@gmail.com>
2025-12-27 18:47:27 +0530

    linear regression example

---
+++ linear_regression.py
@@ -0,0 +1,11 @@
+import numpy as np
+from sklearn.linear_model import LinearRegression
+
+X = np.array([[1], [2], [3], [4], [5]])
+y = np.array([2, 4, 6, 8, 10])
+
+model = LinearRegression()
+model.fit(X, y)
+
+prediction = model.predict([[6]])
+print("Predicted value:", prediction)
